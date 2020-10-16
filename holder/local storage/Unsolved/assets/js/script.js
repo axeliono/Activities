274deffa7bec10ddb3ca50e2c92f8a95ba46fdeb
Axeliono <69375446+axeliono@users.noreply.github.com>
2020-10-15 20:56:12 -0500

    initialize activities repo

---
+++ holder/local storage/Unsolved/assets/js/script.js
@@ -0,0 +1,55 @@
+var emailInput = document.querySelector('#email');
+var passwordInput = document.querySelector('#password');
+var signUpButton = document.querySelector('#sign-up');
+var msgDiv = document.querySelector('#msg');
+var userEmailSpan = document.querySelector('#user-email');
+var userPasswordSpan = document.querySelector('#user-password');
+
+function renderLastRegistered() {
+  // TODO: Retrieve the last email and password from localStorage
+  // TODO: If they are null, return early from this function
+  // TODO: Otherwise set the text of the 'userEmailSpan' and 'userPasswordSpan' to the corresponding values from localStorage
+  var retrievedEmail = localStorage.getItem("email");
+  console.log(retrievedEmail);
+  var retrievedPass = localStorage.getItem("password");
+  console.log(retrievedPass);
+
+  if(userPasswordSpan === null || userEmailSpan === null) {
+    return;
+  }
+
+  userEmailSpan.textContent = retrievedEmail;
+  userPasswordSpan.textContent = retrievedPass;
+
+}
+
+renderLastRegistered();
+
+function displayMessage(type, message) {
+  msgDiv.textContent = message;
+  msgDiv.setAttribute('class', type);
+}
+
+signUpButton.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  var email = document.querySelector('#email').value;
+  var password = document.querySelector('#password').value;
+
+  if (email === '') {
+    displayMessage('error', 'Email cannot be blank');
+  } else if (password === '') {
+    displayMessage('error', 'Password cannot be blank');
+  } else {
+    displayMessage('success', 'Registered successfully');
+
+    // TODO: Save email and password to localStorage
+    // TODO: Render the last registered email and password
+    localStorage.setItem("email",email);
+    localStorage.setItem("password",password);
+    console.log(localStorage.getItem("email")); 
+    console.log(localStorage.getItem("password"));
+
+  }
+  window.location.reload();
+});
